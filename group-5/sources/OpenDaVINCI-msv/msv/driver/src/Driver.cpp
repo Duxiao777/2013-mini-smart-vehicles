4fdd4e8ce28ce4d47644ef8167dac2051a804494
Gokul <gokul@evuri.com>
2013-05-06 06:31:22 +0200

    move 2 units completed

--- group-5/sources/OpenDaVINCI-msv/msv/driver/src/Driver.cpp
+++ group-5/sources/OpenDaVINCI-msv/msv/driver/src/Driver.cpp
@@ -44,147 +44,109 @@ namespace msv {
   
 
 
-        // This method will do the main data processing job.
-        ModuleState::MODULE_EXITCODE Driver::body() {
-	  bool psfound,psfonlyonce; // parking space found, 0 means not found as 1 found
-	  //	  bool frir;
-	   Point3 desiredPosition;
-	   //	   double desiredHeading; // if vehicle turns right the value will decrease and vice versa
-	   //double i = 0;
-	  while (getModuleState() == ModuleState::RUNNING) {
-                // In the following, you find example for the various data sources that are available:
-
-		        // 1. Get most recent vehicle data:
-		        Container containerVehicleData = getKeyValueDataStore().get(Container::VEHICLEDATA);
-		        VehicleData vd = containerVehicleData.getData<VehicleData> ();
-		        // cerr << "Most recent vehicle data: '" << vd.toString() << "'" << endl;
-
-		        // 2. Get most recent sensor board data:
-		        Container containerSensorBoardData = getKeyValueDataStore().get(Container::USER_DATA_0);
-		        SensorBoardData sbd = containerSensorBoardData.getData<SensorBoardData> ();
-		        // cerr << "Most recent sensor board data: '" << sbd.toString() << "'" << endl;
-
-		        // 3. Get most recent user button data:
-		        Container containerUserButtonData = getKeyValueDataStore().get(Container::USER_BUTTON);
-		        UserButtonData ubd = containerUserButtonData.getData<UserButtonData> ();
-			// cerr << "Most recent user button data: '" << ubd.toString() << "'" << endl;
-
-		        // 4. Get most recent steering data as fill from lanedetector for example:
-		        Container containerSteeringData = getKeyValueDataStore().get(Container::USER_DATA_1);
-		        SteeringData sd = containerSteeringData.getData<SteeringData> ();
-		        //cerr << "Most recent steering data: '" << sd.toString() << "'" << endl;
-
-
-		        VehicleControl vc;
-			//double desiredSteeringWheelAngle;
-			 
-			//double rfsen,rbsen,fsen,bsen;
-			//desiredSteeringWheelAngle = 0;
-			
-			// Frot Right and Back right for distance detection 0,2
-			// front and back to adjust to not to hit something   // Safe Value Reading between 16 and 17
-
-			// rfsen = sbd.getDistance(0); //FR IR 4
-			// rbsen = sbd.getDistance(2); //BR IR 5
-			double comp = sbd.getDistance(4) + sbd.getDistance(5);
-						
-			if( (16 < comp) && (comp < 18) ){ // to find parking place  
-			  psfound = 1;
-			}
-			
-			if(!(psfound == 1)){
-			  vc.setSpeed(2);
-			}
-			
-			if((psfound == 1)){ // Parking space found
-			  if(psfonlyonce == 0){ // functions which has to execute only once after found
-			    psfonlyonce = 1;
-			    desiredPosition = vd.getPosition(); // position of car when found parking space
-			    //desiredHeading = vd.getHeading();
-			    vc.setAcceleration(0);
-			    vc.setSpeed(0);
-			    
-			  }
-			  // Replace below two with ,, move two units forward i.e. Y + 2 
-			  // drive until you see a distance range of FRIR more than -1
-			  // if((vd.getVelocity >1)){
-			  //  vc.setAcceleration(0);
-			  
-			  if( vd.getPosition().getX() < desiredPosition.getX()+3 ){ // if current position X is less than desired position
-			    
-			    if( ((sbd.getDistance(1) < 2) || (sbd.getDistance(3) > 2)) && (vd.getHeading() < 1 ) ){ // it CONDITION then only go Backward
-			      	vc.setSteeringWheelAngle(25);
-			  	vc.setSpeed(-0.5);
-			    }
-
-			    else if( ((sbd.getDistance(1) < 2) || (sbd.getDistance(3) > 2)) && (vd.getHeading() > 2 ) ){ // it CONDITION then only go Backward
-			      vc.setSteeringWheelAngle(-25);
-			      vc.setSpeed(-0.5);
-			    }
-			    
-			    else if(((sbd.getDistance(1) > 10) || (sbd.getDistance(3) < 2)) && (vd.getHeading() < 1 )){ // it CONDITION then only go Backward
-			      vc.setSteeringWheelAngle(-25);
-			      vc.setSpeed(0.5);
-			    }
-
-			    else if(((sbd.getDistance(1) > 10) || (sbd.getDistance(3) < 2)) && (vd.getHeading() > 2 )){ // it CONDITION then only go Backward
-			      vc.setSteeringWheelAngle(25);
-			      vc.setSpeed(0.5);
-			    }
-			    
-			    else {
-			      vc.setSteeringWheelAngle(25);
-			      vc.setSpeed(0.5);
-			    }
-			  
-			  
-			  printf("Desired X %f\n", desiredPosition.getX()+3); // position.getX() + 3 = desired X position
-			  printf("Result X %f\n\n", vd.getPosition().getX());
-			  
- 			  printf("IRFR %f\n", sbd.getDistance(0)); 
-			  printf("IRBR %f\n", sbd.getDistance(2)); 
-			  printf("****USF**** %f\n", sbd.getDistance(3));
-			  printf("****IRB**** %f\n", sbd.getDistance(1));
-			  printf("USFR %f\n", sbd.getDistance(4));
-			  printf("USBR %f\n", sbd.getDistance(5));
-			  printf("Heading %f\n",vd.getHeading());
-			  
-			  }
-
-			  printf("Desired X %f\n", desiredPosition.getX()+3); // position.getX() + 3 = desired X position
-			  printf("Result X %f\n\n", vd.getPosition().getX());
-			  
-			}
-			
-			//		    printf("XIN %f\n", position.getX()); // position.getX() + 3 = desired X position
-			//printf("YIN %f\n", position.getY()); // position.getY() = desired Y position
-			
-			//}
-			    //else
-			    //vc.setSpeed(1);  
-			
+  // This method will do the main data processing job.
+  ModuleState::MODULE_EXITCODE Driver::body() {
+    bool psfound,psfonlyonce,move2units; // parking space found, 0 means not found as 1 found
+    //	  bool frir;
+    Point3 desiredPosition;
+    //	   double desiredHeading; // if vehicle turns right the value will decrease and vice versa
+    //double i = 0;
+    while (getModuleState() == ModuleState::RUNNING) {
+      // In the following, you find example for the various data sources that are available:
+      
+      // 1. Get most recent vehicle data:
+      Container containerVehicleData = getKeyValueDataStore().get(Container::VEHICLEDATA);
+      VehicleData vd = containerVehicleData.getData<VehicleData> ();
+      // cerr << "Most recent vehicle data: '" << vd.toString() << "'" << endl;
+      
+      // 2. Get most recent sensor board data:
+      Container containerSensorBoardData = getKeyValueDataStore().get(Container::USER_DATA_0);
+      SensorBoardData sbd = containerSensorBoardData.getData<SensorBoardData> ();
+      // cerr << "Most recent sensor board data: '" << sbd.toString() << "'" << endl;
+      
+      // 3. Get most recent user button data:
+      Container containerUserButtonData = getKeyValueDataStore().get(Container::USER_BUTTON);
+      UserButtonData ubd = containerUserButtonData.getData<UserButtonData> ();
+      // cerr << "Most recent user button data: '" << ubd.toString() << "'" << endl;
+      
+      // 4. Get most recent steering data as fill from lanedetector for example:
+      Container containerSteeringData = getKeyValueDataStore().get(Container::USER_DATA_1);
+      SteeringData sd = containerSteeringData.getData<SteeringData> ();
+      //cerr << "Most recent steering data: '" << sd.toString() << "'" << endl;
+      
+      
+      VehicleControl vc;
+      //double desiredSteeringWheelAngle;
+      
+      //double rfsen,rbsen,fsen,bsen;
+      //desiredSteeringWheelAngle = 0;
+      
+      // Frot Right and Back right for distance detection 0,2
+      // front and back to adjust to not to hit something   // Safe Value Reading between 16 and 17
+      
+      // rfsen = sbd.getDistance(0); //FR IR 4
+      // rbsen = sbd.getDistance(2); //BR IR 5
+      double comp = sbd.getDistance(4) + sbd.getDistance(5);
+      
+      if( (16 < comp) && (comp < 18) ){ // to find parking place  
+	psfound = 1;
+      }
+      
+      if(!(psfound == 1)){
+	vc.setSpeed(2);
+      }
+      
+      if((psfound == 1)){ // Parking space found
+	if(psfonlyonce == 0){ // functions which has to execute only once after found
+	  psfonlyonce = 1;
+	  desiredPosition = vd.getPosition(); // position of car when found parking space
+	  //desiredHeading = vd.getHeading();
 	  
-	  // 4 degree but SteeringWheelAngle expects the angle in radians!
 	  
-			//	        vc.setSteeringWheelAngle(1);			
-
-			
-			    //			    position = vd.getPosition();
-
-			    vc.setBrakeLights(false);
-			vc.setLeftFlashingLights(false);
-			vc.setRightFlashingLights(false);
-			
-			
-			
-		        // Create container for finally sending the data.
-		        Container c(Container::VEHICLECONTROL, vc);
-		        // Send container.
-		        getConference().send(c);
-	        }
-
-	        return ModuleState::OKAY;
-        }
+	}
+	/*Start move two units forward i.e. Y + 2 */ 
+	if((move2units == 0)&&(desiredPosition.getY() + 2 > vd.getPosition().getY())){
+	  vc.setSpeed(1);
+	  
+	}
+	else if((move2units == 0)&&(desiredPosition.getY() + 2 < vd.getPosition().getY())){
+	  move2units = 1;
+	  vc.setAcceleration(0);
+	  vc.setSpeed(0);
+	}
+	/* end move two uints */
+	
+	
+      }
+      
+      
+      printf("Desired X %f\n", desiredPosition.getX()+3); // position.getX() + 3 = desired X position
+      printf("Result X %f\n\n", vd.getPosition().getX());
+      
+      printf("IRFR %f\n", sbd.getDistance(0)); 
+      printf("IRBR %f\n", sbd.getDistance(2)); 
+      printf("****USF**** %f\n", sbd.getDistance(3));
+      printf("****IRB**** %f\n", sbd.getDistance(1));
+      printf("USFR %f\n", sbd.getDistance(4));
+      printf("USBR %f\n", sbd.getDistance(5));
+      printf("Heading %f\n",vd.getHeading());
+      
+      
+      
+      vc.setBrakeLights(false);
+      vc.setLeftFlashingLights(false);
+      vc.setRightFlashingLights(false);
+      
+      
+      
+      // Create container for finally sending the data.
+      Container c(Container::VEHICLECONTROL, vc);
+      // Send container.
+      getConference().send(c);
+    }
+    
+    return ModuleState::OKAY;
+  }
 } // msv
 
 
